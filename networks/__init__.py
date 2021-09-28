a4e6149e868fda0c5c2b69316037fdd530e14b8a
nnzzll <nnzzll@zju.edu.cn>
2021-09-28 16:38:14 +0800

    Add nnUnet

--- networks/__init__.py
+++ networks/__init__.py
@@ -2,5 +2,6 @@ from .unet import UNet
 from .unetpp import UNetPP
 from .unetppp import UNetPPP
 from .unet3d import UNet3D
+from .nnunet import nnUNet
 from .cascade import CascadeUNet
 from .vnet import VNet
